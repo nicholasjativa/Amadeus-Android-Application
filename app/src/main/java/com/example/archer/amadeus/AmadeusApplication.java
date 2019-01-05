fe8957064bf12f9798652acfb9859f44627ad805
Nicholas Jativa <nicholas.jativa@outlook.com>
2019-01-04 22:23:28 -0500

    Added retry policyfor Volley requests

--- app/src/main/java/com/example/archer/amadeus/AmadeusApplication.java
+++ app/src/main/java/com/example/archer/amadeus/AmadeusApplication.java
@@ -2,6 +2,7 @@ package com.example.archer.amadeus;
 
 import android.app.Application;
 
+import com.android.volley.DefaultRetryPolicy;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.Volley;
@@ -9,6 +10,7 @@ import com.android.volley.toolbox.Volley;
 public class AmadeusApplication extends Application {
     public static String AMADEUS_API_URL;
     private static AmadeusApplication instance;
+    private static int MY_SOCKET_TIMEOUT_MS = 3000;
     private RequestQueue queue;
 
     @Override
@@ -34,6 +36,11 @@ public class AmadeusApplication extends Application {
 
     public void addToRequestQueue(Request req) {
 
+        req.setRetryPolicy(new DefaultRetryPolicy(
+            MY_SOCKET_TIMEOUT_MS,
+            DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+            DefaultRetryPolicy.DEFAULT_BACKOFF_MULT
+        ));
         getRequestQueue().add(req);
     }
 }
